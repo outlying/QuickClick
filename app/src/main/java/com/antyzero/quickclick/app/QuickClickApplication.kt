e46c7fd3d0d368d3c06284d043f21fb3d3e82512
Iwo Polański <outlying@gmail.com>
2020-03-06 17:05:34 +0100

    Initial commit

---
+++ app/src/main/java/com/antyzero/quickclick/app/QuickClickApplication.kt
@@ -0,0 +1,12 @@
+package com.antyzero.quickclick.app
+
+import android.app.Application
+
+class QuickClickApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+
+    }
+}
